72426fcf8de9a32392738976a9d5e543ab049bf8
root <root@DESKTOP-R1BEV7I.localdomain>
2026-03-27 10:51:41 +0800

    feat: add edit profile button and pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,31 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def show
-        @user = current_user
+  def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+
+    if params[:user][:avatar].present?
+      @user.avatar.attach(params[:user][:avatar])
     end
+
+    if @user.update(user_params_except_avatar)
+      redirect_to profile_path, notice: "Profile updated successfully!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params_except_avatar
+    params.require(:user).permit(:email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get "pages/about", to: "pages#about"
 
   get "profile", to: "users#show", as: "profile"
+  get "profile/edit", to: "users#edit", as: "edit_profile"
+  patch "profile", to: "users#update"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
